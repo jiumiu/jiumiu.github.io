5d4880afebcdfae781f368c68b8aec8b58268baa
Jiu Kim <jiu.judy@gmail.com>
2024-02-06 17:25:13 -0500

    First commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Toggle the navbar visibility
+function toggleMenu() {
+    var navbar = document.getElementById("navbar");
+    if (navbar.style.display === "block") {
+        navbar.style.display = "none";
+    } else {
+        navbar.style.display = "block";
+    }
+}
