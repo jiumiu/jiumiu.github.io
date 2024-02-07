b55ac66ad208dcf12c241e2ef4b1c4dafa0a8bd8
Jiu Kim <jiu.judy@gmail.com>
2024-02-06 23:58:51 -0500

    Update index.html, styles.css, and scripts.js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-// Toggle the navbar visibility
-function toggleMenu() {
-    var navbar = document.getElementById("navbar");
-    if (navbar.style.display === "block") {
-        navbar.style.display = "none";
-    } else {
-        navbar.style.display = "block";
-    }
-}
+document.addEventListener('DOMContentLoaded', function() {
+    var mobileMenu = document.getElementById('mobile-menu');
+    var navMenu = document.querySelector('.nav-menu');
+    
+    mobileMenu.addEventListener('click', function() {
+        navMenu.classList.toggle('active');
+    });
+
+    document.getElementById('refresh-page').addEventListener('click', function() {
+        window.location.reload(); // 현재 페이지 새로고침
+    });
+});
